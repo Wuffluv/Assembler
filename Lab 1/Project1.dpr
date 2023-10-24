8bbadc70bcbd6dcdcf9cd9fe7420cab9adb17912
Wuffluv <rodion.rycha@yandex.ru>
2023-10-24 19:40:56 +0900

    Add comments for lab 1

--- Lab 1/Project1.dpr
+++ Lab 1/Project1.dpr
@@ -6,14 +6,14 @@ uses
   SysUtils;
 
 var
-a: word;
-b: longword;
+a: word;  //Беззнаковое 16-битное число
+b: longword; //Беззнаковое 32-битное число
 begin
 readln(a);
   asm
-  mov ecx, 0;
-  mov cx, a;
-  mov b, ecx;                                                                   
+  mov ecx, 0; //Инициализируем регистр для работы
+  mov cx, a;  //Загружаем значение в младшие 16 бит регистра
+  mov b, ecx;  //Сохраняем 16-битное значение из 'a' в 'b', преобразованного в 32-битное значение
   end;
 writeln(b);
 readln;
