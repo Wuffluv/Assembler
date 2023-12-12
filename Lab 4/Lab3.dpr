96cb75f8c53b494cb2f1fd3932103907463c0369
Wuffluv <rodion.rycha@yandex.ru>
2023-12-12 22:05:03 +0900

    убрал лишнее

--- Lab 4/Lab3.dpr
+++ Lab 4/Lab3.dpr
@@ -4,15 +4,15 @@ program ProductOfSquares;
 
 var
   n, i: integer;
-  a: array[1..100] of integer; // Массив целых чисел
-  result: integer; // Результат вычислений
+  a: array[1..100] of integer;
+  result: integer;
 
 begin
-  // Ввод количества элементов
-  writeln('Enter the value of n: ');
+
+  writeln('n: ');
   readln(n);
 
-  // Ввод элементов массива
+
   writeln('Enter ', n, ' integers:');
   for i := 1 to n do
   begin
@@ -20,14 +20,14 @@ begin
     readln(a[i]);
   end;
 
-  // Вычисление произведения квадратов с использованием ассемблерного кода
+
   asm
     // Инициализация результата
     mov result, 1
 
     // Цикл по массиву a
-    mov ecx, n        // Регистр ecx используется для хранения количества элементов массива
-    mov esi, 1        // Регистр esi используется для индексации массива
+    mov ecx, n        // Регистр ecx для хранения количества элементов массива
+    mov esi, 1        // Регистр esiдля индексации массива
     @multiplyLoop:
       // Загрузка текущего элемента в регистр ebx
       mov ebx, dword ptr [a + esi * 4]
@@ -48,8 +48,8 @@ begin
       loop @multiplyLoop
   end;
 
-  // Вывод результата
-  writeln('Product of squares: ', result);
+
+  writeln('Result: ', result);
 
   readln;
 end.
